655b2def75885009c6f7ec2d5334e5ac10074614
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-07-24 22:28:43 +0530

    Update LogoOnlyLayout.jsx

--- client/src/layouts/LogoOnlyLayout.jsx
+++ client/src/layouts/LogoOnlyLayout.jsx
@@ -1,16 +1,6 @@
 import { Outlet } from 'react-router-dom';
-
-
-
-// material
 import { styled } from '@mui/material/styles';
 import Logo from '../components/Logo';
-
-
-
-
-// ----------------------------------------------------------------------
-
 const HeaderStyle = styled('header')(({ theme }) => ({
   top: 0,
   left: 0,
