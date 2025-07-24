63c43de2987ab3e79d23eb979131da7ecfca5e3a
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-07-24 22:32:21 +0530

    Update DashboardSidebar.js

--- client/src/layouts/dashboard/DashboardSidebar.js
+++ client/src/layouts/dashboard/DashboardSidebar.js
@@ -1,24 +1,15 @@
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
-// material
 import { styled } from '@mui/material/styles';
-import { Box, Link, Button, Drawer, Typography, Avatar, Stack } from '@mui/material';
+import { Box, Link, Drawer, Typography, Avatar } from '@mui/material';
 import gravatarUrl from 'gravatar-url';
-
-// hooks
 import useResponsive from '../../theme/hooks/useResponsive';
-// components
 import Logo from '../../components/Logo';
 import Scrollbar from '../../components/Scrollbar';
 import NavSection from '../../components/NavSection';
-//
-import navConfig from './NavConfig';
 import Copyright from '../../components/Copyright';
-
 import dataConfig from '../../config.json';
-
-// ----------------------------------------------------------------------
 
 const DRAWER_WIDTH = 280;
 
@@ -37,15 +28,13 @@ const AccountStyle = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.primary['lighter'],
 }));
 
-// ----------------------------------------------------------------------
-
 DashboardSidebar.propTypes = {
   isOpenSidebar: PropTypes.bool,
   onCloseSidebar: PropTypes.func,
 };
 
 export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
-  const user = JSON.parse(localStorage.getItem('profile'))
+  const user = JSON.parse(localStorage.getItem('profile'));
 
   const { pathname } = useLocation();
 
@@ -55,48 +44,47 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
     if (isOpenSidebar) {
       onCloseSidebar();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
 
   const renderContent = (
     <>
-    <Scrollbar
-      sx={{
-        height: 1,
-        '& .simplebar-content': { height: 1, display: 'flex', flexDirection: 'column' },
-      }}
-    >
-      <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>
-        <Logo />
-      </Box>
-
-      <Box sx={{ mb: 5, mx: 2.5, mt: 5 }}>
-        <Link underline="none" component={RouterLink} to={dataConfig.USER_PROFILE_URL}>
-          <AccountStyle>
-          {user&&
-        <Avatar src={gravatarUrl(user?.emailId, {size: 200, default: dataConfig.USER_DEFAULT_LOGO_URL})} alt="photoURL" />}
-            <Box sx={{ ml: 2 }}>
-              <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
-                {user?.firstName} {user?.lastName}
-              </Typography>
-              <Typography variant="span" fontSize={12} sx={{ color: 'text.secondary' }}>
-                {user?.emailId}
-              </Typography>
-            </Box>
-          </AccountStyle>
-        </Link>
-      </Box>
-
-      <NavSection navConfig={navConfig} />
-
-      <Box sx={{ flexGrow: 1 }} /> 
-    </Scrollbar> 
-     <Box sx={{display:'flex', flexDirection: 'column'}} > 
-        <Box sx={{marginTop:'auto', pb:2}}>
-          <Copyright/>
+      <Scrollbar
+        sx={{
+          height: 1,
+          '& .simplebar-content': { height: 1, display: 'flex', flexDirection: 'column' },
+        }}
+      >
+        <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>
+          <Logo />
         </Box>
-      </Box> 
-      </>
+
+        <Box sx={{ mb: 5, mx: 2.5, mt: 5 }}>
+          <Link underline="none" component={RouterLink} to={dataConfig.USER_PROFILE_URL}>
+            <AccountStyle>
+              {user &&
+                <Avatar src={gravatarUrl(user?.emailId, { size: 200, default: dataConfig.USER_DEFAULT_LOGO_URL })} alt="photoURL" />}
+              <Box sx={{ ml: 2 }}>
+                <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
+                  {user?.firstName} {user?.lastName}
+                </Typography>
+                <Typography variant="span" fontSize={12} sx={{ color: 'text.secondary' }}>
+                  {user?.emailId}
+                </Typography>
+              </Box>
+            </AccountStyle>
+          </Link>
+        </Box>
+
+        <NavSection navConfig={navConfig} />
+
+        <Box sx={{ flexGrow: 1 }} />
+      </Scrollbar>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Box sx={{ marginTop: 'auto', pb: 2 }}>
+          <Copyright />
+        </Box>
+      </Box>
+    </>
   );
 
   return (
