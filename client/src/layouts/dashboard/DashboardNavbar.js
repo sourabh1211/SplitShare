13a3f50edca44793f0e1e7e4fcb5c9f6f138debf
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-07-24 22:30:58 +0530

    Update DashboardNavbar.js

--- client/src/layouts/dashboard/DashboardNavbar.js
+++ client/src/layouts/dashboard/DashboardNavbar.js
@@ -1,17 +1,10 @@
 import PropTypes from 'prop-types';
-// material
 import { alpha, styled } from '@mui/material/styles';
 import { Box, Stack, AppBar, Toolbar, IconButton } from '@mui/material';
-// components
 import Iconify from '../../components/Iconify';
-//
 import AccountPopover from './AccountPopover';
-
-import NotificationsPopover from './NotificationsPopover'
-
+import NotificationsPopover from './NotificationsPopover';
 import Searchbar from './Searchbar';
-
-// ----------------------------------------------------------------------
 
 const DRAWER_WIDTH = 280;
 const APPBAR_MOBILE = 64;
@@ -20,7 +13,7 @@ const APPBAR_DESKTOP = 92;
 const RootStyle = styled(AppBar)(({ theme }) => ({
   boxShadow: 'none',
   backdropFilter: 'blur(6px)',
-  WebkitBackdropFilter: 'blur(6px)', // Fix on Mobile
+  WebkitBackdropFilter: 'blur(6px)',
   backgroundColor: alpha(theme.palette.background.default, 0.72),
   [theme.breakpoints.up('lg')]: {
     width: `calc(100% - ${DRAWER_WIDTH + 1}px)`,
@@ -34,8 +27,6 @@ const ToolbarStyle = styled(Toolbar)(({ theme }) => ({
     padding: theme.spacing(0, 5),
   },
 }));
-
-// ----------------------------------------------------------------------
 
 DashboardNavbar.propTypes = {
   onOpenSidebar: PropTypes.func,
@@ -53,7 +44,6 @@ export default function DashboardNavbar({ onOpenSidebar }) {
         <Box sx={{ flexGrow: 1 }} />
 
         <Stack direction="row" alignItems="center" spacing={{ xs: 0.5, sm: 1.5 }}>
-          {/* <NotificationsPopover /> */}
           <AccountPopover />
         </Stack>
       </ToolbarStyle>
