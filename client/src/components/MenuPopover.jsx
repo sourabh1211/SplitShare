6cc11e4935daacd71446912017cdd5510d1afdd7
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-07-25 20:02:10 +0530

    Update MenuPopover.jsx

--- client/src/components/MenuPopover.jsx
+++ client/src/components/MenuPopover.jsx
@@ -1,9 +1,6 @@
 import PropTypes from 'prop-types';
-// material
 import { Popover } from '@mui/material';
 import { alpha, styled } from '@mui/material/styles';
-
-// ----------------------------------------------------------------------
 
 const ArrowStyle = styled('span')(({ theme }) => ({
   [theme.breakpoints.up('sm')]: {
@@ -22,7 +19,7 @@ const ArrowStyle = styled('span')(({ theme }) => ({
   },
 }));
 
-// ----------------------------------------------------------------------
+
 
 MenuPopover.propTypes = {
   children: PropTypes.node.isRequired,
