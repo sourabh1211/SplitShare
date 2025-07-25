f2e2a0c1d34962d3b62e5a4da7b4aa198aa4cd6d
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-07-25 19:44:39 +0530

    Update Iconify.jsx

--- client/src/components/Iconify.jsx
+++ client/src/components/Iconify.jsx
@@ -1,11 +1,6 @@
 import PropTypes from 'prop-types';
-// icons
 import { Icon } from '@iconify/react';
-// @mui
 import { Box } from '@mui/material';
-
-// ----------------------------------------------------------------------
-
 Iconify.propTypes = {
   icon: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
   sx: PropTypes.object,
