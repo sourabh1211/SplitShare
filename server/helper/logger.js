0159079d88571fe49ae034a00ec145ef579e0295
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-08-06 11:55:27 +0530

    Update logger.js

--- server/helper/logger.js
+++ server/helper/logger.js
@@ -7,6 +7,7 @@ const {
 const customFormat = format.combine(format.timestamp(), format.printf((log) => {
     return `${log.timestamp} | ${log.level.toUpperCase().padEnd(7)}| ${log.message}`
 }))
+
 const logger = createLogger({
     format: customFormat,
     transports: [
